9488c95ef69c62df17fa6b9ef214a05c890dee46
unknown <travisallen1983@gmail.com>
2020-06-21 09:50:43 -0500

    confexplorer

---
+++ src/main/kotlin/VideoPlayer.kt
@@ -0,0 +1,77 @@
+import kotlinx.css.*
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.h3
+import react.dom.img
+import styled.css
+import styled.styledButton
+import styled.styledDiv
+
+external interface VideoPlayerProps : RProps {
+    var video: Video
+    var onWatchedButtonPressed: (Video) -> Unit
+    var unwatchedVideo: Boolean
+}
+
+class VideoPlayer : RComponent<VideoPlayerProps, RState>(){
+    override fun RBuilder.render(){
+        styledDiv {
+            css{
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3{
+                + "${props.video.speaker}: ${props.video.title}"
+            }
+            styledButton{
+                css{
+                    display = Display.block
+                    backgroundColor = if(props.unwatchedVideo) Color.lightGreen else Color.red
+                }
+                attrs{
+                    onClickFunction ={
+                        props.onWatchedButtonPressed(props.video)
+                    }
+                }
+                if(props.unwatchedVideo){
+                    +"Mark as watched"
+                }
+                else{
+                    +"Mark as unwatched"
+                }
+            }
+            styledDiv {
+                css{
+                    display = Display.flex
+                    marginBottom = 10.px
+                }
+                EmailShareButton{
+                    attrs.url = props.video.videoUrl
+                    EmailIcon{
+                        attrs.size = 32
+                        attrs.round = true
+                    }
+                }
+                TelegramShareButton{
+                    attrs.url = props.video.videoUrl
+                    TelegramIcon{
+                        attrs.size = 32
+                        attrs.round = true
+                    }
+
+                }
+            }
+
+            ReactPlayer{
+                attrs.url = props.video.videoUrl
+                }
+            }
+        }
+    }
+
+fun RBuilder.videoPlayer(handler: VideoPlayerProps.() -> Unit): ReactElement {
+    return child(VideoPlayer:: class){
+        this.attrs(handler)
+    }
+}
